d8b46cb295adaafa1ef8cdf8193f27e78dab2b94
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-07-26 19:29:36 -0500

    more RESTful endpoints for store and items

--- app.py
+++ app.py
@@ -1,78 +1,107 @@
 from flask import Flask, request
+from db import stores
+from flask_smorest import abort
+
 import uuid
 
 app = Flask(__name__)
 
 
-stores = [
-    {
-        "id": "1",
-        "name": "Music Store",
-        "items": [{"id": "1", "name": "Piano", "price": 2499.99}],
-    }
-]
-
-
 @app.get("/stores")
 def get_stores():
-    return {"stores": stores}
+    return list(stores.values())
 
 
 @app.get("/stores/<string:id>")
 def get_store(id):
-    for store in stores:
-        if store["id"] != id:
-            continue
-        return store
+    try:
+        return stores[id]
+    except KeyError:
+        abort(404, message="Store not found")
 
-    return {"message": "Store not found"}, 404
+
+@app.put("/stores/<string:id>")
+def update_store(id):
+    store_data = request.get_json()
+    try:
+        store = stores[id]
+        store |= store_data
+        return store
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+@app.delete("/stores/<string:id>")
+def delete_store(id):
+    try:
+        del stores[id]
+        return {"message": "Store delete successfully"}
+    except KeyError:
+        abort(404, message="Store not found")
 
 
 @app.post("/stores")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"id": uuid.uuid1(), "name": request_data["name"], "items": []}
-    stores.append(new_store)
+    store_data = request.get_json()
+    id = uuid.uuid4().hex
+    new_store = {**store_data, "id": id}
+    stores[id] = new_store
 
     return new_store, 201
 
 
 @app.get("/stores/<string:id>/items")
 def get_items(id):
-    for store in stores:
-        if store["id"] != id:
-            continue
-        return store["items"]
-
-    return {"message": "Store not found"}, 404
+    try:
+        store = stores[id]
+        return list(store["items"].values())
+    except:
+        abort(404, message="Store not found")
 
 
 @app.get("/stores/<string:id>/items/<string:item_id>")
 def get_item(id, item_id):
-    for store in stores:
-        if store["id"] != id:
-            continue
-        items = store["items"]
-        for item in items:
-            if item["id"] != item_id:
-                continue
-            return item
+    try:
+        store = stores[id]
+        item = store["items"][item_id]
+        return item
+    except KeyError:
+        abort(404, message="Store or Item not found")
 
-    return {"message": "Store not found"}, 404
+
+@app.put("/stores/<string:id>/items/<string:item_id>")
+def update_item(id, item_id):
+    item_data = request.get_json()
+    try:
+        item = stores[id]["items"][item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        abort(404, message="Store or Item not found")
+
+
+@app.delete("/stores/<string:id>/items/<string:item_id>")
+def delete_item(id, item_id):
+    try:
+        store = stores[id]
+        del store["items"][item_id]
+        return {"message": "Item delete successfully"}
+    except KeyError:
+        abort(404, message="Store or Item not found")
 
 
 @app.post("/stores/<string:id>/items")
 def create_item(id):
-    request_data = request.get_json()
-    for store in stores:
-        if store["id"] != id:
-            continue
+    item_data = request.get_json()
+    try:
+        store = stores[id]
+        new_item_id = uuid.uuid4().hex
         new_item = {
-            "id": uuid.uuid1(),
-            "name": request_data["name"],
-            "price": request_data["price"],
+            **item_data,
+            "id": new_item_id,
         }
-        store["items"].append(new_item)
-        return new_item, 201
+        store["items"][new_item_id] = new_item
 
-    return {"message": "Store not found"}, 401
+        return new_item, 201
+    except KeyError:
+        abort(404, message="Store not found")
---
+++ db.py
@@ -0,0 +1,9 @@
+stores = {
+    "1": {
+        "id": "1",
+        "name": "Music Store",
+        "items": {
+            "1": {"id": "1", "name": "Piano", "price": "2999.99"},
+        },
+    }
+}
